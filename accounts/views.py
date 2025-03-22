5aa7ef3a5696e9b629d240dc9f6d719bfa809cd4
patrick-rivett <rivettp@students.dsbn.org>
2025-03-22 16:04:01 -0400

    second change to profile_view

--- accounts/views.py
+++ accounts/views.py
@@ -86,6 +86,11 @@ def login_view(request):
 
 
 # uploading and editing profile screen to cater to what user is online --------------------------------------------
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 @api_view(['GET', 'PUT'])
 @permission_classes([IsAuthenticated])
 def profile_view(request):
