f6738890ee1fddc3924244ad87ff7815c94fe59a
patrick-rivett <rivettp@students.dsbn.org>
2025-03-22 17:40:53 -0400

    changed authentication method

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,17 +1,16 @@
-from django.contrib.auth.backends import BaseBackend
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import User
 
-class MongoEngineBackend(BaseBackend):
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        # 'username' is typically the user ID or email passed by Simple JWT
-        user = User.objects(email=username).first()
-        if user and user.check_password(password):
-            return user
-        return None
-
-    def get_user(self, user_id):
-        # user_id is your ObjectId as a string
+class MongoJWTAuthentication(JWTAuthentication):
+    def get_user(self, validated_token):
+        """
+        Override the default get_user method so that we can use MongoEngine
+        and treat the user ID as a string.
+        """
         try:
+            # Get the user ID from the token using the claim defined in SIMPLE_JWT
+            user_id = validated_token.get(self.get_user_id_claim())
+            # Lookup the user with MongoEngine
             return User.objects.get(id=user_id)
-        except User.DoesNotExist:
+        except Exception as e:
             return None
