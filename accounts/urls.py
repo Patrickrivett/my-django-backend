57a79e8cfa44524b5882b527a8efe2664c258e48
patrick-rivett <rivettp@students.dsbn.org>
2025-03-22 16:58:52 -0400

    edited urls.py file and added profile_view route

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import signup
 from .views import login_view
 from .views import protected_endpoint
+from .views import profile_view
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login_view, name='login'),
+    path('profile/', profile_view, name='profile_view'),
     path('protected/', protected_endpoint, name='protected_endpoint'),
 
 ]
