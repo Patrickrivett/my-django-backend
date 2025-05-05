eda3add92a13cff312945f418cc4c448c4fa4d3c
patrick-rivett <rivettp@students.dsbn.org>
2025-05-05 15:02:24 -0400

    changed apps.py again in accounts

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,19 +1,18 @@
+# backend/accounts/apps.py
 from django.apps import AppConfig
-
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
     def ready(self):
-        # called once at Django startup
+        # Called once, at Django startup, before any auth or view code runs
         import mongoengine
         from django.conf import settings
 
-        # establish your Atlas connection (won’t block on import)
         mongoengine.connect(
             db=settings.MONGO_DB_NAME,
             host=settings.MONGO_HOST,
-            connect=False,                  # defer TCP handshake
-            serverSelectionTimeoutMS=3000,  # 3s max for server-selection
+            connect=False,
+            serverSelectionTimeoutMS=3000,
         )
