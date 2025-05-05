2ca9e46324b96b0e9fce4431462b8053dd92772c
patrick-rivett <rivettp@students.dsbn.org>
2025-05-05 15:19:03 -0400

    made changes to backend

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,18 +1,5 @@
-# backend/accounts/apps.py
 from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-        # Called once, at Django startup, before any auth or view code runs
-        import mongoengine
-        from django.conf import settings
-
-        mongoengine.connect(
-            db=settings.MONGO_DB_NAME,
-            host=settings.MONGO_HOST,
-            connect=False,
-            serverSelectionTimeoutMS=3000,
-        )
--- products/views.py
+++ products/views.py
@@ -1,35 +1,12 @@
-# products/views.py
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-# bring in your Document
 from .models import Product
-
-# bring in mongoengine and settings
-import mongoengine
-from django.conf import settings
-from pymongo import errors
 
 @api_view(['GET'])
 def product_list(request):
     """
     GET /api/products/ → Return all products from MongoDB Atlas
     """
-    # 1) connect on each request (but no import‐time hang)
-    try:
-        mongoengine.connect(
-            db=settings.MONGO_DB_NAME,
-            host=settings.MONGO_HOST,
-            connect=False,                  # defer SRV/DNS until first query
-            serverSelectionTimeoutMS=3000,  # 3 s max for server handshake
-        )
-    except errors.ServerSelectionTimeoutError:
-        return Response(
-            {"error": "Cannot connect to database."},
-            status=503
-        )
-
-    # 2) now do your normal query:
-    docs = Product.objects  # mongoengine QuerySet
+    docs = Product.objects   # uses the single default connection from wsgi.py
     data = [doc.to_mongo().to_dict() for doc in docs]
     return Response(data)
